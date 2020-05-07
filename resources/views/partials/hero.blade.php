c77e7406253709afa7d59e267518ae7601400c70
Monika171 <monikarabha171@gmail.com>
2020-05-08 05:59:30 +0900

    minorChnagesInWelcome.blade.php

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -8,7 +8,7 @@
       <div class="col-xl-10 ftco-animate mb-5 pb-5" data-scrollax=" properties: { translateY: '70%' }">
         <!--<p class="mb-4 mt-5 pt-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We have <span class="number" data-number="850000">0</span> great job offers you deserve!</p>-->
         <p class="mb-4 mt-5 pt-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We Are <!--<span class="number" data-number="850000">0</span>--> Here For You..</p>
-        <h1 class="mb-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We Hold Your Hands <br><span>at this</span> <br><span>Difficult Time..</span></h1>
+        <h1 class="mb-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We Hold Your Hands <br><span>at this <br><span> Difficult Time..</span></h1>
         
         
             <div class="container-fluid h-100">
@@ -21,8 +21,8 @@
                   <div class="card bg-warning text-center p-3">
                     <blockquote class="blockquote mb-0">
                       <h2 class="font-weight-bold text-black-50">***This site is under construction***</h2><br>
-                      <h5 class="font-weight-bold text-black-50">Please WhatsApp on +91-7838883008 for Any Query.</h5><br>
-                      <h5 class="font-weight-bold text-black-50">THANK YOU FOR VISITING!</h5>
+                      <h5 class="font-weight-bold text-black-50">Please WhatsApp on +91-7838883008 for Any Query.</h5><hr>
+                      <h5 class="text-black-50">THANK YOU FOR VISITING!</h5>
                     </blockquote>
                   </div>
                 </div>
