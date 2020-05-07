90e6c1a07b2131f564407a18bd60019d941b401e
Monika171 <monikarabha171@gmail.com>
2020-05-07 18:27:35 +0900

    Some minor changes, added some comments

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -6,7 +6,8 @@
   <div class="container">
     <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
       <div class="col-xl-10 ftco-animate mb-5 pb-5" data-scrollax=" properties: { translateY: '70%' }">
-        <br><br><br><!--<p class="mb-4 mt-5 pt-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We have <span class="number" data-number="850000">0</span> great job offers you deserve!</p>-->
+        <!--<p class="mb-4 mt-5 pt-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We have <span class="number" data-number="850000">0</span> great job offers you deserve!</p>-->
+        <p class="mb-4 mt-5 pt-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We Are <!--<span class="number" data-number="850000">0</span>--> Here For You..</p>
         <h1 class="mb-5" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">We Hold Your Hands <br><span>at this</span> <br><span>Difficult Time..</span></h1>
         
         
@@ -19,8 +20,9 @@
       
                   <div class="card bg-warning text-center p-3">
                     <blockquote class="blockquote mb-0">
-                      <h2 class="font-weight-bold text-black-50">***This site is under construction.***</h2>
-             
+                      <h2 class="font-weight-bold text-black-50">***This site is under construction***</h2><br>
+                      <h4 class="font-weight-bold text-black-50">Please WhatsApp on +91-7838883008 for any query!</h4><br>
+                      <h4 class="font-weight-bold text-black-50">THANK YOU FOR VISITING!</h4>
                     </blockquote>
                   </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,12 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-
+//dummy example (ignore)
 Route::view('demo','demo');
 
-//jobs
+//jobsController for this JobPortal. Basically used to post anything on welcome page. eg: blog, testimonial (irrelevant name sorry!)
 Route::get('/','JobController@index');
+
 //Route::get('/jobs/create','JobController@create')->name('job.create');
 
 //Auth::routes();
@@ -32,6 +33,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 //Route::get('/jobs/{id}/{job}','JobController@show')->name('jobs.show');
 
+
 //company
 Route::get('/company/{id}/{company}','CompanyController@index')->name('company.index');
 Route::get('company/create','CompanyController@create')->name('company.view');
@@ -39,7 +41,7 @@ Route::post('company/create','CompanyController@store')->name('company.store');
 Route::post('company/coverphoto','CompanyController@coverPhoto')->name('cover.photo');
 Route::post('company/logo','CompanyController@companyLogo')->name('company.logo');
 
-//candidate
+//candidate (just one blade.php file. I know I know I have to add more pages like profile! ..later)
 Route::get('user/profile','UserController@index')->name('user.profile');
 Route::post('user/profile/create','UserController@store')->name('profile.create');
 Route::post('user/coverletter','UserController@coverletter')->name('cover.letter');
@@ -50,10 +52,10 @@ Route::post('user/profile_pic','UserController@profile_pic')->name('profile_pic'
 Route::view('employer/register','auth.employer-register')->name('employer.register');
 Route::post('employer/register','EmployerRegisterController@employerRegister')->name('emp.register');
 
-//company
+//See all companies
 Route::get('/companies','CompanyController@company')->name('company');
 
-//admin
+//admin (Only can post something now! Will add more)
 Route::get('/dashboard','DashboardController@index')->middleware('admin');
 Route::get('/dashboard/create','DashboardController@create')->middleware('admin');
 Route::post('/dashboard/create','DashboardController@store')->name('post.store')->middleware('admin');
