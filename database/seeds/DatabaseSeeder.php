6d82bd0e1a55ff727e0d2b1ab49cfb7a50260c2c
Monika171 <monikarabha171@gmail.com>
2020-05-16 04:12:50 +0900

    removedHerokuSeeD

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,16 +22,10 @@ class DatabaseSeeder extends Seeder
         Role::truncate();
 
         $adminRole = Role::create(['name'=>'admin']);
-        /*Role::create(['name'=>'seeker']);
+        Role::create(['name'=>'seeker']);
         Role::create(['name'=>'employer']); 
         Role::create(['name'=>'volunteer']);
-        Role::create(['name'=>'semployer']);*/
-
-        $seekerRole = Role::create(['name'=>'seeker']);
-        $employerRole = Role::create(['name'=>'employer']); 
-        $volunteerRole = Role::create(['name'=>'volunteer']);
         Role::create(['name'=>'semployer']);
-
 
         $admin = User::create([
             'name'=>'admin',
@@ -42,78 +36,8 @@ class DatabaseSeeder extends Seeder
             
         ]);
 
-        $seeker = User::create([
-            'name'=>'seek',
-            'user_type'=>'seeker',
-            'email'=>'seek@gmail.com',
-            'email_verified_at'=>NOW(),
-            'password'=>bcrypt('2020hired')
-            
-        ]);
-
-        Profile::create([
-            'user_id' => $seeker->id,
-            'gender' => 'male',
-            'dob'=>'03/15/2000'
-
-        ]);
-
-        $employer = User::create([
-            'name'=>'emp',
-            'user_type'=>'employer',
-            'email'=>'emp@gmail.com',
-            'email_verified_at'=>NOW(),
-            'password'=>bcrypt('2020hired')
-            
-        ]);
-
-        Company::create([
-            'user_id' => $employer->id,
-            'cname' => 'company1',
-            'slug'=>str_slug('cname')
-
-        ]);
-
-        $volunteer = User::create([
-            'name'=>'volun',
-            'user_type'=>'volunteer',
-            'email'=>'volun@gmail.com',
-            'email_verified_at'=>NOW(),
-            'password'=>bcrypt('2020hired')
-            
-        ]);
-
-        VolunteerProfile::create([
-            'user_id' => $volunteer->id,
-
-        ]);
-
         $admin->roles()->attach($adminRole);
-        $seeker->roles()->attach($seekerRole);
-        $employer->roles()->attach($employerRole);
-        $volunteer->roles()->attach($volunteerRole);
-
-        //Category::truncate();
-        // $this->call(UserSeeder::class);
-        ////factory('App\User',20)->create();
-    	////factory('App\Company',20)->create();
-        //factory('App\Job',20)->create();
-        
-        /*$categories = [
-            
-            'Construction',
-            'Technology',
-            'Engineering',
-            'Software',
-            'Medical',
-            'Government'
-            
-
-        ];
-        foreach($categories as $category){
-            Category::create(['name'=>$category]);
-        }*/
-
+       
 
     }
 }
