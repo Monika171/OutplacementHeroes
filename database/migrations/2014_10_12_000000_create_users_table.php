e5781ad8905e00ec6e1f613e81fc6f86bcdbf70c
Monika171 <monikarabha171@gmail.com>
2020-06-08 08:00:38 +0900

    Added notification field in user table(try1)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('user_type');
+            $table->string('notifications_frequency')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
